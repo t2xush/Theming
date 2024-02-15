fb6f3e44b10a2eb832aa96cce58f2519a4b75266
“ShujuanXu” <t2xush00@students.oamk.fi>
2024-02-15 16:45:05 +0200

    added

---
+++ app/src/main/java/com/example/theming/ui/theme/Shape.kt
@@ -0,0 +1,14 @@
+package com.example.theming.ui.theme
+
+// Inside ui.theme.theme package
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val Shapes = Shapes(
+    small = RoundedCornerShape(0.dp) ,
+    medium = RoundedCornerShape(0.dp) ,
+    large =RoundedCornerShape(0.dp) ,
+    // Add other shape definitions as needed
+)
